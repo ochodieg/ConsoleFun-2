14b50eaf7436a62c663ead6e01e2cb0be52228d5
Ivan <the_real_iori_orochi@hotmail.com>
2020-08-24 08:51:16 -0600

    Add project files.

---
+++ Consolefun.vb
@@ -0,0 +1,31 @@
+﻿Option Strict On
+Option Explicit On
+
+Module Consolefun
+
+    Sub Main()
+        Dim userInput As String
+        ''reserves variable
+        '' " " <-----used to indicate a string rather than a command 
+        'Console.WriteLine("please Type some text then press enter...")
+        'userInput = Console.ReadLine()
+        'Console.WriteLine("Thanks!! You entered: " & userInput & " Good work!!")
+        'Console.ReadLine()
+
+
+        'Dim firstnumber As Integer
+        'Dim result As Integer
+        'Console.WriteLine("please enter a number")
+        'userInput = Console.ReadLine()
+        'firstnumber = CInt(userInput)
+        ''firstnumber = Console.ReadLine()
+        'result = 5 + firstnumber
+
+        'Console.WriteLine(result)
+        'Console.ReadLine()
+
+        Console.Beep(20000, 500)
+
+    End Sub
+
+End Module
